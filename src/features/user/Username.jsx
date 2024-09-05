1e25d575d24f9478f65f1a2692cd1f6f6700c675
madyanes <madyanes.dev@gmail.com>
2024-09-05 19:59:17 +0700

    The box model: spacing, borders, display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className='text-sm font-semibold'>Madyan</div>
+  return <div className='hidden text-sm font-semibold'>Madyan</div>
 }
 
 export default Username
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username'
 
 function Header() {
   return (
-    <header className='bg-yellow-500 uppercase'>
+    <header className='border-b border-stone-200 bg-yellow-500 p-4 uppercase'>
       <Link to='/' className='tracking-widest'>
         Fast React Pizza Co.
       </Link>
