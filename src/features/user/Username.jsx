c6aed7ebc955d0985852a4d70deeb3b6d0edb234
madyanes <madyanes.dev@gmail.com>
2024-09-05 23:00:00 +0700

    Using flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className='hidden text-sm font-semibold'>Madyan</div>
+  return <div className='hidden text-sm font-semibold md:block'>Madyan</div>
 }
 
 export default Username
