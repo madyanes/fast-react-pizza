f039b92a2526de56ac0c33b73b291cde412df945
madyanes <madyanes.dev@gmail.com>
2024-09-05 07:09:33 +0700

    Fetching data with React Router "Loaders"

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable react/prop-types */
+
+import { formatCurrency } from '../../utils/helpers'
+
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza
 
   return (
     <li>
