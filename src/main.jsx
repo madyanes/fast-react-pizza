da18f1cebdf7fa1de1a17eb5ba604cb33561e6c9
madyanes <madyanes.dev@gmail.com>
2024-09-06 23:12:08 +0700

    Fix filename

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 
 import App from './App.jsx'
 import './index.css'
-import store from './store..js'
+import store from './store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
