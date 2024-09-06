b6362b68b53a094c0874308e588a164c0ff06174
madyanes <madyanes.dev@gmail.com>
2024-09-06 08:15:37 +0700

    Styling form elements

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -62,7 +62,12 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type='text' name='address' required />
+            <input
+              type='text'
+              name='address'
+              required
+              className='w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3'
+            />
           </div>
         </div>
 
@@ -73,6 +78,7 @@ function CreateOrder() {
             id='priority'
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
+            className='h-6 w-6 accent-yellow-500 focus-within:ring-offset-2 focus:outline-none focus:ring focus:ring-yellow-400'
           />
           <label htmlFor='priority'>Want to yo give your order priority?</label>
         </div>
